7bfb13c3cded01cbe33330028e68b264f8907e43
RasiSan <rafaeldasilvadossantos8@outlook.com>
2023-02-02 06:24:00 -0300

    Backup
    Menu ônibus finalizado(insert/update)- ainda falta corrigir erro: tentar entrar de novo em um menu após sair de um finaliza conexão com o banco independentemente do input ser int ou str

--- codigo/teste-terminal_rodoviario.py
+++ codigo/teste-terminal_rodoviario.py
@@ -12,9 +12,9 @@ if adm==1:
         print('Carregando...')
         import time
         time.sleep(3)
-        reg1=input('Sistema admnistrativo de empresas\n1-Registro de empresa\n2-Alterar registro\n3-Sair\n')
+        reg1=input('Sistema admnistrativo de empresas\n1-Registrar empresa\n2-Alterar registro\n3-Sair\n')
                 # registrar empresa
-        if reg1=='1':
+        if reg1=="1":
                         print('Sequencia de Registro:CNPJ,nome,telefone,veiculos,rua,bairro,cidade,estado')
                         cnpj=input()
                         nomeempr=input()
@@ -29,7 +29,7 @@ if adm==1:
                         connection.commit()
                         print("Registro de empresa completo")
                     # opção 2 menu empresa/alteração de registro de empresa
-        elif reg1=='2':
+        elif reg1=="2":
                     print('Carregando...')
                     import time
                     time.sleep(1)
@@ -43,7 +43,7 @@ if adm==1:
                     connection.commit()
 
                     # opção 3/returnar ao menu principal)
-        elif reg1=='3':
+        elif reg1=="3":
             print(menu1())
             adm=int(input())
         else:
@@ -59,23 +59,46 @@ if adm==1:
                             print("MySQL finalizado")
 elif adm==2:
     try:
-        print("Qual a placa do veículos")
-        placa=input()
-        print('Qual o destino do veículo')
-        destino=input()
-        print('Quantos assentos comerciais ele possuí?')
-        assentos_comerciais=input()
-        print('Quantos assentos executivos ele possuí?')
-        assentos_executivos=input()
-        print('CNPJ da empresa proprietária')
-        cnpj=input()
+        print('Carregando...')
+        import time
+        time.sleep(3)
+        regon1=int(input('Sistema admnistrativo de ônibus\n1-Registrar Veículo\n2-Alterar registro do veículo\n3-Sair\n'))
+        if regon1==1:
+            print("Qual a placa do veículos")
+            placa=input()
+            print('Qual o destino do veículo')
+            destino=input()
+            print('Quantos assentos comerciais ele possuí?')
+            assentos_comerciais=input()
+            print('Quantos assentos executivos ele possuí?')
+            assentos_executivos=input()
+            print('CNPJ da empresa proprietária')
+            cnpj=input()
 
-        cursor = connection.cursor()
-        insertonib = "INSERT INTO Onibus (Placa,destino,assentos_comerciais,assentos_executivos,Empresa_CNPJ) VALUES (%s, %s, %s, %s,%s)"
-        gravar = (placa,destino,assentos_comerciais,assentos_executivos,cnpj)
-        cursor.execute(insertonib,gravar)
-        connection.commit()
-        print("Inserção bem executada")
+            cursor = connection.cursor()
+            insertonib = "INSERT INTO Onibus (Placa,destino,assentos_comerciais,assentos_executivos,Empresa_CNPJ) VALUES (%s, %s, %s, %s,%s)"
+            gravar = (placa,destino,assentos_comerciais,assentos_executivos,cnpj)
+            cursor.execute(insertonib,gravar)
+            connection.commit()
+            print("Inserção bem executada")
+        elif regon1==2:
+                    print('Carregando...')
+                    import time
+                    time.sleep(1)
+                    print('Alteração de registro de Veiculo')
+                    update1=input('O que alterar?\n')
+                    update2=input('Valor ao ser alterado\n')
+                    
+                    update3=input('Placa do Veículo\n')
+                    mySql_insert_query = f"UPDATE Onibus SET {update1} = '{update2}' WHERE Placa={update3}"
+                    cursor.execute(mySql_insert_query)
+                    connection.commit()
+        elif regon1==3:
+            print(menu1())
+            adm=int(input())
+        else:
+                    print("Opção Invalida")
+
 
     except mysql.connection.Error as error:
                         print("Falha ao registrar empresa{}".format(error))
